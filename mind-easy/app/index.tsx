a8d057b36c00d2d5f0670809db009aceb8063743
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2025-10-22 11:35:57 +0300

    Test project

---
+++ mind-easy/app/index.tsx
@@ -0,0 +1,53 @@
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { useRouter } from "expo-router";
+
+export default function HomeScreen() {
+  const router = useRouter();
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>👋 Привет в MindEasy</Text>
+      <Text style={styles.subtitle}>Твое место для ментального баланса</Text>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => router.push("/tabs")}
+      >
+        <Text style={styles.buttonText}>Перейти к вкладкам</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f5f7fb",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingHorizontal: 20,
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: "600",
+    color: "#333",
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontSize: 16,
+    color: "#666",
+    textAlign: "center",
+    marginBottom: 40,
+  },
+  button: {
+    backgroundColor: "#6C63FF",
+    paddingVertical: 14,
+    paddingHorizontal: 24,
+    borderRadius: 12,
+  },
+  buttonText: {
+    color: "#fff",
+    fontWeight: "600",
+    fontSize: 16,
+  },
+});
