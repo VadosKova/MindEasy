c4b992cebc519e0aae2590f696d48f0be96093e7
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2025-11-12 09:40:02 +0200

    Add theme color constants for light and dark modes

---
+++ mind-easy/constants/theme.ts
@@ -0,0 +1,15 @@
+const tintColorLight = '#0a7ea4';
+const tintColorDark = '#fff';
+
+export const Colors = {
+  light: {
+    text: '#11181C',
+    background: '#fff',
+    tint: tintColorLight,
+  },
+  dark: {
+    text: '#ECEDEE',
+    background: '#151718',
+    tint: tintColorDark,
+  },
+};
