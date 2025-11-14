984fabc9e1412b7e851541cf6c34b7905b7c9c9b
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2025-11-14 20:04:13 +0200

    Set width and link for images

--- mind-easy/app/index.tsx
+++ mind-easy/app/index.tsx
@@ -17,6 +17,14 @@ import { SmileyIcon } from '@/components/icons/SmileyIcon';
 import { HeadphonesIcon } from '@/components/icons/HeadphonesIcon';
 import { JournalIcon } from '@/components/icons/JournalIcon';
 
+const { width: SCREEN_WIDTH } = Dimensions.get('window');
+
+const carouselImages = [
+  { uri: 'https://images.freeimages.com/images/large-previews/9b0/sunset-1344107.jpg' },
+  { uri: 'https://th.bing.com/th/id/R.ab48afc6bf0eefb07e7dd1e30f6dcba8?rik=CAZjNPpZe%2bV0wg&pid=ImgRaw&r=0' },
+  { uri: 'https://img.freepik.com/premium-photo/4k-desktop-wallpaper-hills-river-nature_485374-11875.jpg?w=740' },
+];
+
 export default function OnBoardingScreen() {
   
 
