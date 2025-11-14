ee4355323cd23e8b9502f4af964eef18e170b9a9
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2025-11-14 20:49:11 +0200

    Fixed GradientText

--- mind-easy/components/GradientText.tsx
+++ mind-easy/components/GradientText.tsx
@@ -4,7 +4,7 @@ import { LinearGradient } from 'expo-linear-gradient';
 import MaskedView from '@react-native-masked-view/masked-view';
 
 interface GradientTextProps {
-  colors: string[];
+  colors: readonly [string, string, ...string[]];
   style?: any;
   children: React.ReactNode;
 }
