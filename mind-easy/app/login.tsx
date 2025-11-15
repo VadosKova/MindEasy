cf207a1095a6a5469d5aad752b9822f79c533b9b
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2025-11-15 10:31:51 +0200

    Imported main components

---
+++ mind-easy/app/login.tsx
@@ -0,0 +1,25 @@
+import { useEffect, useRef, useState } from 'react';
+import {
+  Alert,
+  Dimensions,
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { router } from 'expo-router';
+import Svg, { Path, G, ClipPath, Defs, Rect } from 'react-native-svg';
+
+const { width: SCREEN_WIDTH } = Dimensions.get('window');
+
+const carouselImages = [
+  { uri: 'https://images.freeimages.com/images/large-previews/9b0/sunset-1344107.jpg' },
+  { uri: 'https://th.bing.com/th/id/R.ab48afc6bf0eefb07e7dd1e30f6dcba8?rik=CAZjNPpZe%2bV0wg&pid=ImgRaw&r=0' },
+  { uri: 'https://img.freepik.com/premium-photo/4k-desktop-wallpaper-hills-river-nature_485374-11875.jpg?w=740' },
+];
