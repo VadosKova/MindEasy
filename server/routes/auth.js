0a880e3984f6f1f788a65ce31d8410ea6a75af9f
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2026-01-28 16:41:55 +0200

    POST method for authorization

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -27,3 +27,29 @@ router.post("/register", async (req, res) => {
     res.status(500).json({ error: "Registration failed" });
   }
 });
+
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await Users.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({ error: "Authentication failed" });
+    }
+
+    const passwordMatch = await bcryptjs.compare(password, user.password);
+    if (!passwordMatch) {
+      return res.status(401).json({ error: "Authentication failed" });
+    }
+
+    const token = jwt.sign({ userId: user._id, email: user.email }, "Secret", {
+      expiresIn: "1h",
+    });
+    res.status(200).json({  message: "User registered successfully", token });
+  } catch (error) {
+    console.error("Login error:", error.message);
+    res.status(500).json({ error: "Login failed" });
+  }
+});
+
+export default router;
