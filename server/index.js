0a42183aaf2537dc3b8473118ba8faf3c0f04b43
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2026-01-29 11:34:37 +0200

    Updated index.js

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import cors from 'cors';
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.js";
 
@@ -7,6 +8,7 @@ dotenv.config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 
