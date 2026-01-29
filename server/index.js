c2978c2cf3bb586dfb193e66d49ac62040cb9b39
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2026-01-29 20:08:19 +0200

    Added journal api

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from 'cors';
 import authRoutes from "./routes/auth.js";
+import journalRoutes from "./routes/journal.js";
 
 dotenv.config();
 
@@ -11,6 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
+app.use("/api/journal", journalRoutes);
 
 mongoose
   .connect(process.env.MONGO_URI)
