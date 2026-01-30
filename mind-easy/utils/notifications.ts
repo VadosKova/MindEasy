8f3e0a4abccd9ff4f18002046626607362329c97
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2026-01-30 19:32:33 +0200

    Permission request

---
+++ mind-easy/utils/notifications.ts
@@ -0,0 +1,15 @@
+import * as Notifications from 'expo-notifications';
+import { Platform } from 'react-native';
+
+export async function requestNotificationPermission() {
+  const { status } = await Notifications.getPermissionsAsync();
+
+  if (status !== 'granted') {
+    const { status: newStatus } =
+      await Notifications.requestPermissionsAsync();
+
+    return newStatus === 'granted';
+  }
+
+  return true;
+}
