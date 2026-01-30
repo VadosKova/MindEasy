26d6775bd7d05d3c20b1550e00f5b4188bfa07e5
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2026-01-30 11:32:36 +0200

    Connection with index.js

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from 'cors';
 import authRoutes from "./routes/auth.js";
+import quoteRoutes from "./routes/quotes.js";
 import journalRoutes from "./routes/journal.js";
 
 dotenv.config();
@@ -12,6 +13,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
+app.use("/api/quotes", quoteRoutes);
 app.use("/api/journal", journalRoutes);
 
 mongoose
