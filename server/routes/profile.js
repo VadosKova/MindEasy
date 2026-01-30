42458897377fb90564322632bdca026b67b6b2bb
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2026-01-30 12:15:25 +0200

    Added PUT method for profile

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -9,3 +9,23 @@ router.get("/", authMiddleware, async (req, res) => {
   const user = await User.findById(req.userId).select("-password");
   res.json(user);
 });
+
+router.put("/", authMiddleware, async (req, res) => {
+  const { username, whyUseApp, goals, reminders, avatar } = req.body;
+
+  const user = await User.findByIdAndUpdate(
+    req.userId,
+    {
+      username,
+      whyUseApp,
+      goals,
+      reminders,
+      avatar,
+    },
+    { new: true }
+  ).select("-password");
+
+  res.json(user);
+});
+
+export default router;
