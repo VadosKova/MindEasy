ba1c7d5d3b482e0544dc855eef4ff679fe129c47
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2026-02-06 10:19:38 +0200

    Imported default libraries

---
+++ mind-easy/app/tabs/calm-breathing.tsx
@@ -0,0 +1,8 @@
+import { useEffect, useRef, useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { useRouter } from 'expo-router';
+import { BackIcon } from '@/components/icons/BackIcon';
+
+const TOTAL_TIME = 300;
+const PHASE_TIME = 4;
