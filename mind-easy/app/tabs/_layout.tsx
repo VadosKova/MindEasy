6ae3139011b191e1fc886264e52702cddc822050
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2026-02-07 13:05:02 +0200

    Commented route

--- mind-easy/app/tabs/_layout.tsx
+++ mind-easy/app/tabs/_layout.tsx
@@ -54,12 +54,6 @@ export default function TabsLayout() {
           tabBarIcon: ({ focused }) => <TabProfileIcon size={36} active={focused} />,
         }}
       />
-      <Tabs.Screen
-        name="calm-breathing"
-        options={{
-          href: null,
-        }}
-      />
     </Tabs>
   );
 }
