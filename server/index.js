eef73c97761442421d7a3f310bd086ee07dd8313
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2026-02-07 16:33:35 +0200

    Imported route for meditation

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ import authRoutes from "./routes/auth.js";
 import moodRoutes from "./routes/mood.js";
 import progressRoutes from "./routes/progress.js";
 import quoteRoutes from "./routes/quotes.js";
+import meditationRoutes from "./routes/meditation.js";
 import journalRoutes from "./routes/journal.js";
 import profileRoutes from "./routes/profile.js";
 import { createAdminIfNotExists } from "./utils/createAdmin.js";
@@ -22,6 +23,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/mood", moodRoutes);
 app.use("/api/progress", progressRoutes);
 app.use("/api/quotes", quoteRoutes);
+app.use("/api/meditation", meditationRoutes);
 app.use("/api/journal", journalRoutes);
 app.use("/api/profile", profileRoutes);
 
