013e73f766f6726f398c149135ca5aab2be3fd0a
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2026-02-09 00:31:14 +0200

    Updated POST method for audio file

--- server/routes/journal.js
+++ server/routes/journal.js
@@ -1,4 +1,6 @@
 import express from "express";
+import fs from "fs";
+import path from "path";
 import JournalEntry from "../models/JournalEntry.js";
 import authMiddleware from "../middleware/authMiddleware.js";
 
@@ -6,7 +8,19 @@ const router = express.Router();
 
 router.post("/", authMiddleware, async (req, res) => {
   try {
-    const { mood, text, audioUrl } = req.body;
+    const { mood, text, audioBase64 } = req.body;
+
+    let audioUrl = "";
+
+    if (audioBase64) {
+      const fileName = `audio_${Date.now()}.m4a`;
+      const filePath = path.join("uploads", fileName);
+
+      const base64Data = audioBase64.replace(/^data:audio\/\w+;base64,/, "");
+      fs.writeFileSync(filePath, base64Data, "base64");
+
+      audioUrl = `http://192.168.88.15:5000/uploads/${fileName}`;
+    }
 
     const entry = new JournalEntry({
       user: req.userId,
