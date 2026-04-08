73050743a69ebe8fc566ba6617bcd4e682a7b2c5
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2026-04-08 17:31:06 +0300

    Created RainScene

--- mind-easy/app/sleep&relax/scenes/RainScene.tsx
+++ mind-easy/app/sleep&relax/scenes/RainScene.tsx
@@ -1,3 +1,15 @@
+import { StyleSheet } from 'react-native';
+import { Video, ResizeMode } from 'expo-av';
+
 export default function RainScene() {
-  //test
+  return (
+    <Video
+      source={require('@/assets/videos/rain.mp4')}
+      style={StyleSheet.absoluteFill}
+      resizeMode={ResizeMode.COVER}
+      shouldPlay
+      isLooping
+      isMuted
+    />
+  );
 }
