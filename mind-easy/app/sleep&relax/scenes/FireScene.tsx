945b10dd67d4920e0b296d27bd599d695b9307f2
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2026-04-08 17:38:19 +0300

    Created FireScene

--- mind-easy/app/sleep&relax/scenes/FireScene.tsx
+++ mind-easy/app/sleep&relax/scenes/FireScene.tsx
@@ -1,3 +1,15 @@
+import { StyleSheet } from 'react-native';
+import { Video, ResizeMode } from 'expo-av';
+
 export default function FireScene() {
-  //test
+  return (
+    <Video
+      source={require('@/assets/videos/fire.mp4')}
+      style={StyleSheet.absoluteFill}
+      resizeMode={ResizeMode.COVER}
+      shouldPlay
+      isLooping
+      isMuted
+    />
+  );
 }
