f0ae6e0093e8d8d8616cd662ca953d482f134505
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2026-05-29 12:43:39 +0300

    Completed icons

--- mind-easy/components/icons/LeaveIcon.tsx
+++ mind-easy/components/icons/LeaveIcon.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import Svg, { Path } from 'react-native-svg';
 
-export function LeaveIcon() {
+interface LeaveIconProps {
+  size?: number;
+  color?: string;
+}
+
+export function LeaveIcon({ size = 26, color = "white" }: LeaveIconProps) {
   return (
-    <Svg width={26} height={26} viewBox="0 0 26 26" fill="none">
-      <Path d="M9.20837 23.2917C9.20837 23.2917 16.25 23.8333 20.0417 19.5C23.8334 15.1667 23.8334 2.16667 23.8334 2.16667C23.8334 2.16667 10.8334 1.625 6.50004 4.875C2.16671 8.125 3.25004 17.3333 3.25004 17.3333" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-      <Path d="M2.16671 23.8333C2.16671 23.8333 8.22216 19.257 11.9167 15.1666C15.6113 11.0762 17.3334 7.04163 17.3334 7.04163" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-      <Path d="M11.9167 15.1667L11.3751 8.125" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-      <Path d="M11.9167 15.1667L17.3334 14.625" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <Svg width={size} height={size} viewBox="0 0 26 26" fill="none">
+      <Path d="M9.20837 23.2917C9.20837 23.2917 16.25 23.8333 20.0417 19.5C23.8334 15.1667 23.8334 2.16667 23.8334 2.16667C23.8334 2.16667 10.8334 1.625 6.50004 4.875C2.16671 8.125 3.25004 17.3333 3.25004 17.3333" stroke={color} stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      <Path d="M2.16671 23.8333C2.16671 23.8333 8.22216 19.257 11.9167 15.1666C15.6113 11.0762 17.3334 7.04163 17.3334 7.04163" stroke={color} stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      <Path d="M11.9167 15.1667L11.3751 8.125" stroke={color} stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      <Path d="M11.9167 15.1667L17.3334 14.625" stroke={color} stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
     </Svg>
   );
 }
