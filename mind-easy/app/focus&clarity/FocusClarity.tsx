7ca1f548581dfd46180681ba994067874a05e232
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2026-05-29 12:39:23 +0300

    Updated params for icons and fixed style for focus and clarity

--- mind-easy/app/focus&clarity/FocusClarity.tsx
+++ mind-easy/app/focus&clarity/FocusClarity.tsx
@@ -32,7 +32,7 @@ export default function FocusClarityScreen() {
         <View style={[styles.pomodoroCard]}> 
           <View style={styles.pomodoroHeader}>
             <View style={styles.alarmCircle}>
-              <AlarmClockIcon />
+              <AlarmClockIcon size={38} color="#FFFFFF" />
             </View>
             <View style={styles.pomodoroTextBlock}>
               <Text style={[styles.pomodoroTitle]}>Pomodoro Timer</Text>
@@ -54,14 +54,14 @@ export default function FocusClarityScreen() {
           <View style={styles.actionRow}>
             <TouchableOpacity style={[styles.actionButton, styles.focusButton]}>
               <View style={styles.actionIconWrap}>
-                <StartFocusIcon />
+                <StartFocusIcon size={17} color="#FFFFFF" />
               </View>
               <Text style={styles.actionText}>Focus (25 min)</Text>
             </TouchableOpacity>
 
             <TouchableOpacity style={[styles.actionButton, styles.breakButton]}>
               <View style={styles.actionIconWrap}>
-                <CoffeeIcon />
+                <CoffeeIcon size={20} color="#FFFFFF" />
               </View>
               <Text style={styles.actionText}>Break (5 min)</Text>
             </TouchableOpacity>
@@ -141,7 +141,7 @@ const styles = StyleSheet.create({
     width: 54,
     height: 54,
     borderRadius: 28,
-    backgroundColor: '#3C8DAD',
+    backgroundColor: '#2980C2',
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -190,9 +190,10 @@ const styles = StyleSheet.create({
     gap: 12,
   },
   actionButton: {
+    height: 50,
     flex: 1,
     padding: 16,
-    borderRadius: 20,
+    borderRadius: 30,
     flexDirection: 'row',
     alignItems: 'center',
     gap: 12,
@@ -208,8 +209,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   actionText: {
-    fontFamily: 'IstokWeb_700Bold',
-    fontSize: 14,
+    fontFamily: 'Jua_400Regular',
+    fontSize: 16,
     color: '#FFFFFF',
   },
   featureCard: {
@@ -241,10 +242,10 @@ const styles = StyleSheet.create({
     marginTop: 25,
   },
   featureTitle: {
-    fontFamily: 'IstokWeb_700Bold',
+    fontFamily: 'Jua_400Regular',
     fontSize: 18,
     color: '#102A43',
-    marginTop: -15,
+    marginTop: -20,
   },
   featureDescription: {
     width: '70%',
@@ -254,7 +255,7 @@ const styles = StyleSheet.create({
     color: '#33475B',
     marginBottom: 16,
     marginLeft: 62,
-    marginTop: -30,
+    marginTop: -40,
   },
   featureButton: {
     width: 140,
